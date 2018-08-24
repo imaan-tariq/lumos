44513f309bbf411809ed30250df4718201551fa0
Janette <janette_lopez@prodigy.net.mx>
2018-08-23 22:19:53 -0700

    added a basic layout to the contacts activity. The info needs to be saved the in Firebase.

--- app/src/main/java/com/lumos/lumos/AddContacts.java
+++ app/src/main/java/com/lumos/lumos/AddContacts.java
@@ -2,12 +2,18 @@ package com.lumos.lumos;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class AddContacts extends AppCompatActivity {
+public class AddContacts extends AppCompatActivity implements View.OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_contacts);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
